824b90e039853e581677ebb1288eafa7c1406cf8
Antonio Pierro <antonio.pierro@gmail.com>
2021-11-19 17:00:40 +0100

    chore: use async queue, queue.drain instead of Promise.all

--- src/store-programs-id.js
+++ src/store-programs-id.js
@@ -1,19 +1,26 @@
+import async from 'async';
 import { getProgramsFromBlock } from './programs.utils.js';
 import * as utils from './utils.js';
 
-const storeProgramIds = async (slot, length = 1) => {
-    const { json2csv, uniq, getProgramsId } = utils;
-    const new_programs = [];
-    const blocks = Array.from({ length }, (_, i) => i + slot);
-    const old_programs = await getProgramsId();
-    const promises = blocks.map(async (e) => {
-        const programs = await getProgramsFromBlock(e);
-        console.log('block number:', e);
-        new_programs.push(...programs);
-    });
+const new_programs = [];
 
-    await Promise.all(promises);
-    json2csv(uniq([...old_programs, ...new_programs]).map((e) => [e]));
+const storeProgramIds = async (slot, length = 1) => {
+    const blocks = Array.from({ length }, (_, i) => i + slot);
+    blocks.forEach(async (blockId) => queue.push(blockId));
 };
 
-storeProgramIds(107_300_100, 20);
+const queue = async.queue(async function (blockId) {
+    const programs = await getProgramsFromBlock(blockId);
+    console.log(`finished processing block ${blockId}`);
+    new_programs.push(...programs);
+}, 1);
+
+queue.drain(async function () {
+    const { json2csv, uniq, getProgramsId } = utils;
+    const old_programs = await getProgramsId();
+    console.log('all items have been processed');
+    json2csv(uniq([...old_programs, ...new_programs]).map((e) => [e]));
+});
+
+// 107_300_000->107_300_400
+storeProgramIds(107_300_300, 100);
