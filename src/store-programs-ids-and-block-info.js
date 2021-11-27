c8d2bb52daa2a74e2595699488651324c0729bce
Antonio Pierro <antonio.pierro@gmail.com>
2021-11-27 09:48:03 +0100

    feat: add stream to store blocks

--- src/store-programs-ids-and-block-info.js
+++ src/store-programs-ids-and-block-info.js
@@ -1,34 +1,50 @@
 import async from 'async';
 import { getBlockData, storeBlockInfo, getBlocksIds } from './block.utils.js';
 import * as utils from './utils.js';
+import { config } from './config.js';
+import fs from 'fs';
+const { getProgramsId } = utils;
 
-const new_programs = [];
+let stream, prevBlocks, prevPrograms;
+
+const callbackBlock = async (blockId) => {
+    if (prevBlocks.indexOf(blockId) !== -1) return null;
+    queue.push(blockId);
+};
+
+const callbackProgam = async (newProgram) => {
+    if (prevPrograms.indexOf(newProgram) !== -1) return null;
+    stream.write(`${newProgram}\n`);
+    prevPrograms.push(newProgram);
+};
 
 const storeProgramIdsAndBlockInfo = async (slot, length = 1) => {
     const blocks = Array.from({ length }, (_, i) => i + slot);
-    const prevBlocks = await getBlocksIds();
-    blocks.forEach(async (blockId) => {
-        if (prevBlocks.indexOf(blockId) === -1) {
-            queue.push(blockId);
-        }
-    });
+    const callbackStrem = () => blocks.forEach((e) => callbackBlock(e));
+    stream.once('open', callbackStrem);
 };
 
-const queue = async.queue(async function (blockId) {
+const queue = async.queue(async (blockId) => {
     const { programs, block } = await getBlockData(blockId);
     storeBlockInfo(block, blockId);
     console.log(`finished processing block ${blockId}`);
-    new_programs.push(...programs);
+    programs.forEach((e) => callbackProgam(e));
 }, 1);
 
 queue.drain(async function () {
-    const { json2csv, uniq, getProgramsId } = utils;
-    const old_programs = await getProgramsId();
     console.log('all items have been processed');
-    json2csv(uniq([...old_programs, ...new_programs]).map((e) => [e]));
+    stream.end();
 });
 
-// 90_000_000 -> 90_000_500
-// 100_000_000-> 100_000_500
-// 107_300_000->107_300_500
-storeProgramIdsAndBlockInfo(107_300_000, 5_000);
+(async () => {
+    prevBlocks = await getBlocksIds();
+    prevPrograms = await getProgramsId();
+    stream = fs.createWriteStream(config.program_fn, { flags: 'a' });
+    // 90_000_000 -> 90_000_500
+    // 100_000_000-> 100_000_500
+    // 107_300_000->107_300_500
+    storeProgramIdsAndBlockInfo(101_000_000, 200);
+})();
+
+
+
